b970b722b35dffbd4979e1273c554bcf3c3e48eb
virajt71 <soulr27@gmail.com>
2023-05-25 22:42:15 +0200

    style: added comments

--- linux_setup/configuring-tools.sh
+++ linux_setup/configuring-tools.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
+echo "update asdf"
 asdf update
 
+echo "add aws-vault"
 asdf plugin add aws-vault
+
+echo "add awscli"
 asdf plugin add awscli
+
+echo "add terraform"
 asdf plugin add terraform
+
+echo "add terragrunt"
 asdf plugin add terragrunt
