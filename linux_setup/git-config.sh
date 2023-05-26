87378d1f3ffe5487f7d8332883f63f1bc558a09a
virajt71 <soulr27@gmail.com>
2023-05-26 18:22:51 +0200

    Update git config script

--- linux_setup/git-config.sh
+++ linux_setup/git-config.sh
@@ -3,14 +3,35 @@
 echo "Git Configuration Setup"
 echo "-----------------------"
 
+# Set Git configuration settings
+git config --global core.editor "vim"
+git config --global color.ui true
+# git config --global alias.st status
+git config --global push.default current
+git config --global credential.helper 'manager-core'
+
 # Prompt for user input
 read -p "Enter your name: " name
 read -p "Enter your email: " email
 
-# Set Git configuration
+# Set name & email
 git config --global user.name "$name"
 git config --global user.email "$email"
 
-echo "Git configuration has been set:"
-echo "Name: $name"
-echo "Email: $email"
+# Set gpg
+git config --global gpg.program "gpg"
+git config --global commit.gpgSign true
+
+# Check GPG configuration
+gpg_check=$(git config --global user.signingkey)
+if [ -n "$gpg_check" ]; then
+    echo "GPG configuration is set up correctly."
+else
+    gpg --full-generate-key
+    gpg --list-secret-keys --keyid-format=long
+    read -p "Enter your signing key: " key
+    gpg --armor --export "$key"
+    echo "Copy the exported key and add it under github gpg"
+fi
+
+git config --list
