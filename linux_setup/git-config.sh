7ff8061ac4d7057f9191bf4d5de9a1141c6a55fa
virajt71 <soulr27@gmail.com>
2023-05-27 03:37:36 +0200

    added password

--- linux_setup/git-config.sh
+++ linux_setup/git-config.sh
@@ -7,16 +7,22 @@ echo "-----------------------"
 git config --global core.editor "vim"
 git config --global color.ui true
 # git config --global alias.st status
-git config --global push.default current
+# git config --global push.default current
+git config --global core.autocrlf 'false'
 git config --global credential.helper 'manager-core'
+git config --global credential.credentialStore 'gpg'
+git config --global commit.gpgsign 'true'
 
 # Prompt for user input
 read -p "Enter your name: " name
 read -p "Enter your email: " email
+echo "genrate personal token from developer in setting"
+read -p "Enter your password: " password
 
 # Set name & email
 git config --global user.name "$name"
 git config --global user.email "$email"
+git config --global user.password "$password"
 
 # Set gpg
 git config --global gpg.program "gpg"
@@ -35,4 +41,3 @@ else
 fi
 
 git config --list
-
