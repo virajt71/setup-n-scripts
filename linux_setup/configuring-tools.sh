8e7d7a62b2c72614be2d0a8fce70364b348ca1ad
virajt71 <soulr27@gmail.com>
2023-06-05 17:06:04 +0200

    replace aws-vault with aws-sso-cli

--- linux_setup/configuring-tools.sh
+++ linux_setup/configuring-tools.sh
@@ -3,23 +3,22 @@ echo "update asdf"
 asdf update
 
 echo "......................"
-for tool in awscli aws-vault terraform terragrunt;
+for tool in awscli aws-sso-cli terraform terragrunt;
 do
   echo "-> Adding plugin $tool"
   asdf plugin add "$tool" 
 done
 
 echo "......................"
-for tool in awscli aws-vault terraform terragrunt;
+for tool in awscli aws-sso-cli terraform terragrunt;
 do
   echo "-> Installing $tool"
   asdf install "$tool" latest
 done
 
+
 echo "......................"
-asdf global aws-vault 7.2.0
-echo "......................"
-for tool in awscli terraform terragrunt;
+for tool in awscli aws-sso-cli terraform terragrunt;
 do
   echo "-> Global $tool"
   asdf global "$tool" latest
