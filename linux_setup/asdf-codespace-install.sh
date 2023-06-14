8e20079ec742de2f672f92d267db3bc1f875ada6
virajt71 <soulr27@gmail.com>
2023-06-14 23:05:59 +0200

    Wip (#2)
    * refactor: add lib
    
    * feat: terraform template
    
    * fix: region move to eu-central-1
    
    * fix permission
    
    * Create asdf-codespace-install.sh
    
    * Update global values
    
    * added git shortcut
    
    * Update asdf-codespace-install.sh
    
    * Update asdf-codespace-install.sh
    
    * replace aws-vault with aws-sso-cli
    
    * added local
    
    * Update configuring-tools.sh
    
    * Update asdf-codespace-install.sh
    
    * add local time

---
+++ linux_setup/asdf-codespace-install.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+echo "........................................"
+echo "update & upgrade"
+sudo apt-get update && sudo apt-get upgrade -y
+sudo apt-get -y install xdg-utils
+
+echo "........................................"
+echo "Install apt modules"
+sudo apt-get install -y curl git
+sudo apt-get install -y unzip
+sudo apt-get install -y pass gpg
+
+echo "........................................"
+echo "Instal asdf"
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.11.3
+
+echo "........................................"
+echo "set local time"
+sudo unlink /etc/localtime
+sudo ln -s /usr/share/zoneinfo/Europe/Warsaw /etc/localtime
+
+echo "........................................"
+echo "alias cl='clear'" >> ~/.bashrc
+echo "alias add='git add '" >> ~/.bashrc
+echo "alias commit='git commit -m '" >> ~/.bashrc
+echo "alias push='git push'" >> ~/.bashrc
+echo "alias fetch='git fetch'" >> ~/.bashrc
+echo "alias pull='git pull'" >> ~/.bashrc
+echo "alias status='git status'" >> ~/.bashrc
+echo "alias diff='git diff'" >> ~/.bashrc
+echo "alias tf='terraform'" >> ~/.bashrc
+echo "alias tg='terragrunt'" >> ~/.bashrc
+
+echo "........................................"
+echo "Run following"
+echo ". "$HOME/.asdf/asdf.sh" >> ~/.bashrc"
+echo ". "$HOME/.asdf/completions/asdf.bash" >> ~/.bashrc"
+echo "source ~/.bashrc"
+echo "asdf update"
--- linux_setup/configuring-tools.sh
+++ linux_setup/configuring-tools.sh
@@ -3,17 +3,29 @@ echo "update asdf"
 asdf update
 
 echo "......................"
-for tool in awscli aws-vault terraform terragrunt;
+for tool in awscli aws-sso-cli terraform terragrunt;
 do
   echo "-> Adding plugin $tool"
   asdf plugin add "$tool" 
 done
 
 echo "......................"
-for tool in awscli aws-vault terraform terragrunt;
+for tool in awscli aws-sso-cli terraform terragrunt;
 do
   echo "-> Installing $tool"
   asdf install "$tool" latest
 done
 
-echo "Run 'asdf <global|shell|local> <tools> <version>' to set version "
+echo "......................"
+for tool in awscli aws-sso-cli terraform terragrunt;
+do
+  echo "-> Global $tool"
+  asdf global "$tool" latest
+done
+
+echo "......................"
+for tool in awscli aws-sso-cli terraform terragrunt;
+do
+  echo "-> local $tool"
+  asdf local "$tool" latest
+done
