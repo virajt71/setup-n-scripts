d8f353892aa2457903fb1a244a16f988a4cf4f30
viraj tamakuwala <soulr27@gmail.com>
2023-12-19 11:53:00 +0530

    feat: new

---
+++ linux_setup/ubuntu install script/prometheus.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+echo ".............."
+echo "Install trivy"
+echo ".............."
+sudo apt update
+sudo useradd \
+    --system \
+    --no-create-home \
+    --shell /bin/false prometheus
+wget https://github.com/prometheus/prometheus/releases/download/v2.48.1/prometheus-2.48.1.linux-amd64.tar.gz
+tar -xvf prometheus-2.48.1.linux-amd64.tar.gz
+sudo mkdir -p /data /etc/prometheus
+cd prometheus-2.32.1.linux-amd64
+sudo mv prometheus promtool /usr/local/bin/
+sudo mv consoles/ console_libraries/ /etc/prometheus/
+sudo mv prometheus.yml /etc/prometheus/prometheus.yml
+sudo chown -R prometheus:prometheus /etc/prometheus/ /data/
+cd
+rm -rf prometheus*
+prometheus --version
+
+cat <<EOF > /etc/systemd/system/prometheus.service
+[Unit]
+Description=Prometheus
+Wants=network-online.target
+After=network-online.target
+
+StartLimitIntervalSec=500
+StartLimitBurst=5
+
+[Service]
+User=prometheus
+Group=prometheus
+Type=simple
+Restart=on-failure
+RestartSec=5s
+ExecStart=/usr/local/bin/prometheus \
+  --config.file=/etc/prometheus/prometheus.yml \
+  --storage.tsdb.path=/data \
+  --web.console.templates=/etc/prometheus/consoles \
+  --web.console.libraries=/etc/prometheus/console_libraries \
+  --web.listen-address=0.0.0.0:9090 \
+  --web.enable-lifecycle
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl daemon-reload
+systemctl enable prometheus.service
+systemctl start prometheus.service
