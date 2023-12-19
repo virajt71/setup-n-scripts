0177aa59bab8c40eb626fb8f493964d051326c5b
viraj tamakuwala <soulr27@gmail.com>
2023-12-19 15:21:59 +0530

    refactor: update command

--- linux_setup/ubuntu install script/jenkins.sh
+++ linux_setup/ubuntu install script/jenkins.sh
@@ -20,8 +20,6 @@ echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
 sudo apt-get update
-sudo apt-get install jenkins
+sudo apt-get install jenkins -y
 sudo systemctl start jenkins
 sudo systemctl enable jenkins
-
-source ~/.bashrc
--- linux_setup/ubuntu install script/trivy.sh
+++ linux_setup/ubuntu install script/trivy.sh
@@ -3,8 +3,8 @@
 echo ".............."
 echo "Install trivy"
 echo ".............."
-sudo apt-get install wget apt-transport-https gnupg lsb-release
+sudo apt-get install wget apt-transport-https gnupg lsb-release -y
 wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | sudo apt-key add -
 echo deb https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main | sudo tee -a /etc/apt/sources.list.d/trivy.list
 sudo apt-get update
-sudo apt-get install trivy
+sudo apt-get install trivy -y
