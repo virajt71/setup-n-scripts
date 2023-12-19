f5a507cfb9eaa628093202110de82ed07bbffd75
viraj tamakuwala <soulr27@gmail.com>
2023-12-19 12:10:31 +0530

    fix: update prometheus

--- linux_setup/ubuntu install script/prometheus.sh
+++ linux_setup/ubuntu install script/prometheus.sh
@@ -11,7 +11,7 @@ sudo useradd \
 wget https://github.com/prometheus/prometheus/releases/download/v2.48.1/prometheus-2.48.1.linux-amd64.tar.gz
 tar -xvf prometheus-2.48.1.linux-amd64.tar.gz
 sudo mkdir -p /data /etc/prometheus
-cd prometheus-2.32.1.linux-amd64
+cd prometheus-2.48.1.linux-amd64
 sudo mv prometheus promtool /usr/local/bin/
 sudo mv consoles/ console_libraries/ /etc/prometheus/
 sudo mv prometheus.yml /etc/prometheus/prometheus.yml
