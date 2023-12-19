0ad59dd5f280532535c30098cc8b332ff8ac23cd
viraj tamakuwala <soulr27@gmail.com>
2023-12-19 13:29:00 +0530

    fix: fix links

--- linux_setup/ubuntu install script/download_all_scripts.sh
+++ linux_setup/ubuntu install script/download_all_scripts.sh
@@ -1,8 +1,5 @@
-wget https://github.com/virajt71/setup-n-scripts/blob/main/linux_setup/ubuntu%20install%20script/docker.sh
-wget https://github.com/virajt71/setup-n-scripts/blob/main/linux_setup/ubuntu%20install%20script/jenkins.sh
-wget https://github.com/virajt71/setup-n-scripts/blob/main/linux_setup/ubuntu%20install%20script/node_exporter.sh
-wget https://github.com/virajt71/setup-n-scripts/blob/main/linux_setup/ubuntu%20install%20script/prometheus.sh
-wget https://github.com/virajt71/setup-n-scripts/blob/main/linux_setup/ubuntu%20install%20script/trivy.sh
-
-chmod +x docker.sh jenkins.sh node_exporter.sh prometheus.sh trivy.sh
-
+sh -c "$(curl -fsSL https://github.com/virajt71/setup-n-scripts/blob/main/linux_setup/ubuntu%20install%20script/docker.sh)"
+sh -c "$(curl -fsSL https://github.com/virajt71/setup-n-scripts/blob/main/linux_setup/ubuntu%20install%20script/jenkins.sh)"
+sh -c "$(curl -fsSL https://github.com/virajt71/setup-n-scripts/blob/main/linux_setup/ubuntu%20install%20script/node_exporter.sh)"
+sh -c "$(curl -fsSL https://github.com/virajt71/setup-n-scripts/blob/main/linux_setup/ubuntu%20install%20script/prometheus.sh)"
+sh -c "$(curl -fsSL https://github.com/virajt71/setup-n-scripts/blob/main/linux_setup/ubuntu%20install%20script/trivy.sh)"
