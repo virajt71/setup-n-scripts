3a9ff1cf879972ad84e18f7b2459eada85b48172
viraj tamakuwala <soulr27@gmail.com>
2023-12-19 13:53:13 +0530

    refactor: updated script

--- linux_setup/ubuntu install script/download_all_scripts.sh
+++ linux_setup/ubuntu install script/download_all_scripts.sh
@@ -1,5 +1,30 @@
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/virajt71/setup-n-scripts/main/linux_setup/ubuntu%20install%20script/docker.sh)"
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/virajt71/setup-n-scripts/main/linux_setup/ubuntu%20install%20script/jenkins.sh)"
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/virajt71/setup-n-scripts/main/linux_setup/ubuntu%20install%20script/node_exporter.sh)"
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/virajt71/setup-n-scripts/main/linux_setup/ubuntu%20install%20script/prometheus.sh)"
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/virajt71/setup-n-scripts/main/linux_setup/ubuntu%20install%20script/trivy.sh)"
+#!/bin/bash
+
+declare -A options=(
+    [1]="docker.sh"
+    [2]="jenkins.sh"
+    [3]="node_exporter.sh"
+    [4]="prometheus.sh"
+    [5]="trivy.sh"
+)
+
+echo "Choose options to run (separate by spaces):"
+echo "1. Docker installation"
+echo "2. Jenkins installation"
+echo "3. Node Exporter installation"
+echo "4. Prometheus installation"
+echo "5. Trivy installation"
+
+read -p "Enter your choices (1-5, separated by spaces): " choices
+
+# Split the input choices into an array
+read -r -a choices_array <<< "$choices"
+
+# Loop through the choices and execute the selected scripts
+for choice in "${choices_array[@]}"; do
+    if [[ "${options[$choice]}" ]]; then
+        sh -c "$(curl -fsSL https://raw.githubusercontent.com/virajt71/setup-n-scripts/main/linux_setup/ubuntu%20install%20script/${options[$choice]})"
+    else
+        echo "Invalid choice: $choice"
+    fi
+done
