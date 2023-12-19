d2673dec775427adef4a2200c5ae352f7bc2eb39
viraj tamakuwala <soulr27@gmail.com>
2023-12-19 13:18:37 +0530

    feat: all scipts download

---
+++ linux_setup/ubuntu install script/download_all_scripts.sh
@@ -0,0 +1,8 @@
+wget https://github.com/virajt71/setup-n-scripts/blob/main/linux_setup/ubuntu%20install%20script/docker.sh
+wget https://github.com/virajt71/setup-n-scripts/blob/main/linux_setup/ubuntu%20install%20script/jenkins.sh
+wget https://github.com/virajt71/setup-n-scripts/blob/main/linux_setup/ubuntu%20install%20script/node_exporter.sh
+wget https://github.com/virajt71/setup-n-scripts/blob/main/linux_setup/ubuntu%20install%20script/prometheus.sh
+wget https://github.com/virajt71/setup-n-scripts/blob/main/linux_setup/ubuntu%20install%20script/trivy.sh
+
+chmod +x docker.sh jenkins.sh node_exporter.sh prometheus.sh trivy.sh
+
