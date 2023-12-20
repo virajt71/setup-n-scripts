a47bf8a7633334cc5e9ccf201e21a0f413499d11
viraj tamakuwala <soulr27@gmail.com>
2023-12-20 16:13:42 +0530

    refactor: cleaning

--- linux_setup/ubuntu install script/docker.sh
+++ linux_setup/ubuntu install script/docker.sh
@@ -6,12 +6,9 @@ echo ".............."
 sudo apt update && sudo apt upgrade -y
 sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo -n "$x"
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-echo -n "$x"
 apt-cache policy docker-ce
 sudo apt install docker-ce -y
 sudo usermod -aG docker $USER
 newgrp docker
 sudo chmod 777 /var/run/docker.sock
-echo -n "$x"
--- linux_setup/ubuntu install script/grafana.sh
+++ linux_setup/ubuntu install script/grafana.sh
@@ -5,13 +5,9 @@ echo "Install grafana"
 echo ".............."
 sudo apt-get update
 sudo apt-get install -y apt-transport-https software-properties-common
-echo -n "$x"
 wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
 echo "deb https://packages.grafana.com/oss/deb stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
-echo -n "$x"
 sudo apt-get update
 sudo apt-get -y install grafana
-echo -n "$x"
 sudo systemctl enable grafana-server
 sudo systemctl start grafana-server
-echo -n "$x"
